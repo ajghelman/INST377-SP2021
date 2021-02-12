c3279284d31b8855ac6679525437a3b658ff6c41
ajghelman <78034606+ajghelman@users.noreply.github.com>
2021-02-12 14:36:06 -0500

    lab 3 sort of works

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,21 @@
-/* Put your javascript in here */
+    let list = display.querySelector('ul');
+    let listElems = display.querySelectorAll('li');
+	const onigiri = document.querySelectorAll('.images');
+    const array1 = Array.from(onigiri);
+    let start = 0; 
+    
+    array1.forEach((element) => {
+        console.log(element);
+    })
+
+    display.querySelector('.left').onclick = function leftArrow() {
+      start += 130 * 3;
+      start = Math.min(start, 0)
+      list.style.marginLeft = start + 'px';
+    };
+
+    display.querySelector('.right').onclick = function rightArrow() {
+      start -= 130 * 3;
+      start = Math.max(start, -130 * (listElems.length - 3));
+      list.style.marginLeft = start + 'px';
+    };
